0c31b848e65b73586742188ab743a4be10b01a9a
MitchelMA <32548@ma-web.nl>
2025-02-25 12:09:39 +0100

    addition of the intersection node which can be used by traverses for easy navigation

---
+++ ManPac/Assets/Scripts/Traversing/IntersectionNode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class IntersectionNode : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2[] Directions = new[]
+    {
+        new Vector2(0, 1),
+        new Vector2(1, 0),
+        new Vector2(0, -1),
+        new Vector2(-1, 0)
+    };
+
+    private void OnValidate()
+    {
+        int directionCount = Directions.Length;
+        for (var i = 0; i < directionCount; i++)
+            Directions[i].Normalize();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        int directionCount = Directions.Length;
+        Vector3 position = transform.position;
+        for (var i = 0; i < directionCount; i++)
+        {
+            var offsetPosition = position + new Vector3(Directions[i].x, 0, Directions[i].y);
+            Gizmos.DrawLine(position, offsetPosition);
+            Gizmos.DrawSphere(offsetPosition, 0.1f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 0.3f);
+    }
+
+
+    public Vector3 GetDirection(Vector2 preference)
+    {
+        preference.Normalize();
+        
+        float bestValue = -1;
+        int bestIndex = 0;
+
+        int directionCount = Directions.Length;
+        for (var i = 0; i < directionCount; i++)
+        {
+            var currentDirection = Directions[i];
+            var currentBestValue = Vector2.Dot(currentDirection, preference);
+            if (currentBestValue > bestValue)
+            {
+                bestValue = currentBestValue;
+                bestIndex = i;
+            }
+            
+            if (Mathf.Abs(bestValue - 1) < Mathf.Epsilon)
+                break;
+        }
+
+        return Directions[bestIndex];
+    }
+}
