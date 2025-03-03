263db1f553b28ca3f71b7c27bec1c4e461bb0a63
MitchelMA <32548@ma-web.nl>
2025-03-03 15:46:40 +0100

    Addition of the power-pellet and starting with animation

--- ManPac/Assets/Scripts/Pickups/Pellet.cs
+++ ManPac/Assets/Scripts/Pickups/Pellet.cs
@@ -1,15 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 public class Pellet : MonoBehaviour
 {
+    [Header("Score settings")]
     [SerializeField]
     private PelletTypes PelletType = PelletTypes.Ordinary;
-
     [SerializeField]
     private int PelletScore = 10;
     
     public UnityEvent<int, PelletTypes> OnPickedUp;
+
+    [Header("AnimationSettings")]
+    [SerializeField]
+    private Animator Animator;
+    
     private void OnTriggerEnter(Collider manpac)
     {
         if (manpac.gameObject.CompareTag("ManPac"))
@@ -17,5 +24,15 @@ public class Pellet : MonoBehaviour
             OnPickedUp.Invoke(PelletScore, PelletType);
             Destroy(this.gameObject);
         }
+    }
+
+    private void Start()
+    {
+        Invoke(nameof(EnableAnimator), Random.value);
+    }
+
+    private void EnableAnimator()
+    {
+        Animator.enabled = true;
     }
 }
