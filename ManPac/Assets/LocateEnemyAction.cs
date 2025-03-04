477d1dd88202624a06454f40f804d6c281b22cfc
daan41114 <rendomgamer41114@gmail.com>
2025-03-04 15:43:11 +0100

    it now tracks the difference between the positions of the player and the Man-Pac

--- ManPac/Assets/LocateEnemyAction.cs
+++ ManPac/Assets/LocateEnemyAction.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using Unity.Behavior;
+using Unity.Mathematics;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using Math = Unity.Mathematics.Geometry.Math;
 
 [Serializable, GeneratePropertyBag]
 [NodeDescription(name: "Locate Enemy ", story: "Locate [Enemy] for [Player]", category: "Action", id: "f9a7ac1f7c0041f79049651e54d89bb1")]
@@ -10,9 +13,15 @@ public partial class LocateEnemyAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Enemy;
     [SerializeReference] public BlackboardVariable<GameObject> Player;
-
+    private GameObject _player;
+    private GameObject _enemy;
+    private List<float> positionDifferents = new();
     protected override Status OnStart()
     {
+        _player = GameObject.Find("Player");
+        _enemy = GameObject.Find("ManPac");
+        LocateEnemy();
+
         return Status.Running;
     }
 
@@ -27,10 +36,16 @@ public partial class LocateEnemyAction : Action
 
     private void LocateEnemy()
     {
-        GameObject player = Player.Value;
-        GameObject enemy = Enemy.Value;
-        Vector2 positionDifference = player.transform.position - enemy.transform.position;
-        Debug.Log(positionDifference);
+        positionDifferents.Clear();
+        Vector2 playerPos = _player.transform.position;
+        Vector2 enemyPos = _enemy.transform.position;
+        positionDifferents.Add(playerPos.x - enemyPos.x);
+        positionDifferents.Add(playerPos.y - enemyPos.y);
+    }
+
+    private void GetBiggerDifference()
+    {
+        
     }
     
 }
