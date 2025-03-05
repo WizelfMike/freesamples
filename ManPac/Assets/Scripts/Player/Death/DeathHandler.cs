0c41a06a769a104a8d905a48654d41b4d1bbda3c
WizelfMike <mikeyclarke.com@gmail.com>
2025-03-05 10:41:34 +0100

    Fully linked the death system with the Power Pellet states.

--- ManPac/Assets/Scripts/Player/Death/DeathHandler.cs
+++ ManPac/Assets/Scripts/Player/Death/DeathHandler.cs
@@ -13,9 +13,16 @@ public class DeathHandler : MonoBehaviour
     private PlayerMovement _thisPlayerMovement;
     private IntersectionTraverser _thisPlayerTraverser;
     private bool _isActivePlayer = true;
+    private bool _isInDanger;
+    private bool _canDie;
+
+    public bool IsInDanger { private get => _isInDanger; set => _isInDanger = value; }
+    public bool CanDie { get => _canDie; set => _canDie = value; }
 
     private void Start()
     {
+        _canDie = true;
+        _isInDanger = false;
         _thisPlayerTraverser = GetComponent<IntersectionTraverser>();
         _thisPlayerMovement = GetComponent<PlayerMovement>();
         _thisPlayerInput = GetComponent<PlayerInput>();
@@ -28,20 +35,18 @@ public class DeathHandler : MonoBehaviour
         }
     }
 
-    private void Update() // will be removed once the true method of calling death is functional
-    {
-        if(Input.GetKeyDown(KeyCode.J))
-        {
-            CallDeath();
-        }
-    }
-
     public void CallDeath()
     {
-        StartCoroutine(Death());
+        if (_canDie == true)
+        {
+            Debug.Log("Death Happened");
+            StartCoroutine(Death());
+            return;
+        }
     }
     private IEnumerator Death()
     {
+        _canDie = false;
         _thisPlayerInput.enabled = false;
         _thisPlayerTraverser.CurrentVelocity = 0f;
         GhostAnimator.SetTrigger("Died");
@@ -56,6 +61,6 @@ public class DeathHandler : MonoBehaviour
 
         _thisPlayerInput.enabled = true;
         _thisPlayerTraverser.CurrentVelocity = 1f;
-        
+        _canDie = true;
     }
 }
--- ManPac/Assets/Scripts/Player/Death/GhostStateHandler.cs
+++ ManPac/Assets/Scripts/Player/Death/GhostStateHandler.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GhostStateHandler : MonoBehaviour
 {
-    
+    private DeathHandler thisDeathHandler;
+    private void Start()
+    {
+        thisDeathHandler = GetComponent<DeathHandler>();
+    }
+
+    public void ChangeToScaredState(ManPacStates currentState)
+    {
+        if (currentState == ManPacStates.Aggressive)
+        {
+            thisDeathHandler.IsInDanger = true;
+        }
+
+        if (currentState == ManPacStates.Avoidant)
+        {
+            thisDeathHandler.IsInDanger = false;
+        }
+    }
 }
