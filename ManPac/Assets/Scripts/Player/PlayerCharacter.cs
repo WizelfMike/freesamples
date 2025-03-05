7ed69f788a90caea036e1376819e6cfb5e6e9106
MitchelMA <32548@ma-web.nl>
2025-03-05 10:51:23 +0100

    Addition of Player switching system with distinct player types

---
+++ ManPac/Assets/Scripts/Player/PlayerCharacter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerCharacterTypes CharacterType;
+    [Description("Broadcasts when an activation change took place, the boolean argument is the current active state")]
+    public UnityEvent<bool> OnActivationChange;
+
+    public PlayerCharacterTypes Type => CharacterType;
+
+    private bool _isPlayerControlled = false;
+    private PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    public bool PlayerDeactivate()
+    {
+        if (!_isPlayerControlled)
+            return false;
+        
+        _playerInput.DeactivateInput();
+        _isPlayerControlled = false;
+        
+        // TODO! Placeholder code is here for now,
+        // Fill up with Behaviour-AI to take over control.
+        
+        Debug.Log($"Player character `{gameObject.name}` got deactivated, AI is going to take over control");
+        OnActivationChange.Invoke(_isPlayerControlled);
+        return true;
+    }
+
+    public bool PlayerActivate()
+    {
+        if (_isPlayerControlled)
+            return false;
+        
+        _playerInput.ActivateInput();
+        _isPlayerControlled = true;
+        
+        // TODO! Placeholder code is here for now,
+        // Fill up with code to deactivate the Behaviour-AI so the player can take back control.
+        Debug.Log($"Player character `{gameObject.name}` got activated, the player is granted back control");
+        OnActivationChange.Invoke(_isPlayerControlled);
+        return true;
+    }
+}
---
+++ ManPac/Assets/Scripts/Player/Switching/CharacterSwitcher.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class CharacterSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerCharacterTypes StartCharacter = PlayerCharacterTypes.Pinky;
+    [SerializeField]
+    private PlayerCharacter[] PlayerCharacters;
+    public UnityEvent<PlayerCharacter> OnCharacterActivated;
+
+    private PlayerCharacter _currentPlayerActiveCharacter = null;
+    private int _characterTypeCount = 0;
+
+    private void Start()
+    {
+        _characterTypeCount = Enum.GetValues(typeof(PlayerCharacterTypes)).Length;
+        PrepareAllCharacters(StartCharacter);
+    }
+
+    public void InputSwitchNextPlayer(InputAction.CallbackContext context)
+    {
+        if (_currentPlayerActiveCharacter == null)
+            return;
+
+        int typeIndex = (int) _currentPlayerActiveCharacter.Type;
+        int nextIndex = (typeIndex + 1) % _characterTypeCount;
+        
+        PrepareNextCharacter((PlayerCharacterTypes) nextIndex);
+    }
+
+    private PlayerCharacter PrepareAllCharacters(PlayerCharacterTypes type)
+    {
+        int characterCount = PlayerCharacters.Length;
+        PlayerCharacter activated = default;
+        for (int i = 0; i < characterCount; i++)
+        {
+            PlayerCharacter playerCharacter = PlayerCharacters[i];
+            if (playerCharacter.Type == type)
+            {
+                playerCharacter.PlayerActivate();
+                activated = playerCharacter;
+                continue;
+            }
+
+            playerCharacter.PlayerDeactivate();
+        }
+
+        _currentPlayerActiveCharacter = activated;
+        OnCharacterActivated.Invoke(activated);
+        return activated;
+    }
+
+    private PlayerCharacter PrepareNextCharacter(PlayerCharacterTypes type)
+    {
+        if (_currentPlayerActiveCharacter.Type == type)
+            return _currentPlayerActiveCharacter;
+        
+        PlayerCharacter next = PlayerCharacters.First(x => x.Type == type);
+        
+        if (!next.PlayerActivate())
+            return _currentPlayerActiveCharacter;
+        
+        _currentPlayerActiveCharacter.PlayerDeactivate();
+        _currentPlayerActiveCharacter = next;
+        
+        OnCharacterActivated.Invoke(_currentPlayerActiveCharacter);
+        
+        return _currentPlayerActiveCharacter;
+    }
+}
